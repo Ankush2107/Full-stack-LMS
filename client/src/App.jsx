b65235b3d94444cac81cf0c0dd10ef22351a62a6
Ankush2107 <ankush.kumar210701@gmail.com>
2023-12-31 12:18:24 +0530

    creating file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 
 import { Routes } from 'react-router-dom';
+
+import Footer from './components/Footer.jsx';
 function App() {
 
   return (
-    <Routes>
-      {/* <Route path='/' element={<Home/>}></Route> */}
-    </Routes>
+    // <Routes>
+    //   {/* <Route path='/' element={<Home/>}></Route> */}
+      
+    // </Routes>
+    <Footer/>
   )
 }
 
