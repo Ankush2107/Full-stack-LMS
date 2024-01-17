64b4aab68a0d76a089d745f9f0f0edcb6fbf46f4
Ankush2107 <ankush.kumar210701@gmail.com>
2024-01-17 13:12:27 +0530

    client side is completed

--- client/src/Redux/store.jsx
+++ client/src/Redux/store.jsx
@@ -1,11 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
-import authSliceReducer from './Slices/AuthSlice';
+import authReducer from './slices/authSlice';
+import courseReducer from './slices/courseSlice';
+import lectureReducer from './slices/lectureSlice';
+import razorpayReducer from './slices/razorPaySlice';
+
 
 const store = configureStore({
     reducer: {
-        auth: authSliceReducer
+        auth: authReducer,
+        course: courseReducer,
+        razorpay: razorpayReducer,
+        lecture: lectureReducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
     devTools: true
 });
 
--- client/src/layouts/HomeLayouts.jsx
+++ client/src/layouts/HomeLayouts.jsx
@@ -1,76 +1,122 @@
 import { AiFillCloseCircle} from 'react-icons/ai';
 import { FiMenu } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 
-import Footer from '../components/Footer'
+import Footer from "../components/Footer";
+import { logout } from '../redux/slices/authSlice';
 
-function HomeLayouts({ childern }) {
 
-   // Function to change the width of the drawer-side element to 'auto'
-   function changeWidth() {
-        // Get the elements with the class name 'drawer-side'
-        const drawerSide = document.getElementsByClassName('drawer-side');
-        
-        // Set the width of the drawer-side element to 'auto'
+function HomeLayouts({ children }) {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+    const role = useSelector((state) => state?.auth?.role);
+
+    function changeWidth() {
+        const drawerSide = document.getElementsByClassName("drawer-side");
         drawerSide[0].style.width = 'auto';
     }
 
-    // Function to hide the drawer by unchecking the drawer-toggle input and setting the width of drawer-side to '0'
     function hideDrawer() {
-        // Get the elements with the class name 'drawer-toggle'
         const element = document.getElementsByClassName("drawer-toggle");
-        
-        // Uncheck the drawer-toggle input to hide the drawer
         element[0].checked = false;
 
-        // Get the elements with the class name 'drawer-side'
-        const drawerSide = document.getElementsByClassName('drawer-side');
-        
-        // Set the width of the drawer-side element to '0' to hide it
+        const drawerSide = document.getElementsByClassName("drawer-side");
         drawerSide[0].style.width = '0';
     }
 
+    async function onLogout(e) {
+        e.preventDefault();
+
+        const response = await dispatch(logout());
+        if(response?.payload?.data)
+            navigate("/");
+    }
+
     return (
+        
         <div className="min-h-[90vh]">
             <div className="drawer absolute left-0 z-50 w-full">
                 <input id="my-drawer" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
-                    {/* Page content here */}
-                    <label htmlFor="my-drawer" >
+                    <label htmlFor="my-drawer">
                         <FiMenu onClick={changeWidth} size={"32px"} className='font-bold text-white m-4' />
                     </label>
-                </div> 
+                </div>
                 <div className="drawer-side w-0">
                     <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                        {/* Sidebar content here */}
-                        <li>
+                    <ul className='menu p-4 w-48 h-[100%] sm:w-80 bg-base-200 text-base-content relative'>
+                        <li className='w-fit absolute right-2 z-50'>
                             <button onClick={hideDrawer}>
                                 <AiFillCloseCircle size={24}/>
                             </button>
                         </li>
                         <li>
-                            <Link to='/'>Home</Link>
+                            <Link to="/"> Home </Link>
+                        </li>
+                        {isLoggedIn && role === "ADMIN" && (
+                            <li>
+                                <Link to="/admin/dashboard">Admin Dashboard</Link>
+                            </li>
+                        )
+
+                        }
+                        {isLoggedIn && role === "ADMIN" && (
+                            <li>
+                                <Link to="/course/create">Create Course</Link>
+                            </li>
+                        )
+
+                        }
+                        <li>
+                            <Link to="/about"> About us </Link>
                         </li>
                         <li>
-                            <Link to='/about'>About us</Link>
+                            <Link to="/contact"> Contact us </Link>
                         </li>
                         <li>
-                            <Link to='/contact'>Contact us</Link>
+                            <Link to="/courses"> All courses </Link>
                         </li>
-                        <li>
-                            <Link to='/courses'>All courses</Link>
-                        </li>
+
+                        {!isLoggedIn ? (
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to="/signin">Login</Link>
+                                    </button>
+                                    <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to="/signup">Signup</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        ) : (
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to="/user/profile">Profile</Link>
+                                    </button>
+                                    <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link onClick={onLogout}>Logout</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )
+
+                        }
                     </ul>
                 </div>
             </div>
 
-            {childern}
+            {children}
 
             <Footer />
-            
+
         </div>
-    )
+        
+    );
 }
 
 export default HomeLayouts;
