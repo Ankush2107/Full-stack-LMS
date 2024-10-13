5abf89dc1f6cca4ee5efeef2a340c49ac453676a
Ankush2107 <ankush.kumar210701@gmail.com>
2024-10-13 22:49:21 +0530

    Added EditProfile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,50 @@
-import './App.css';
+import "./App.css";
 
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom"
 
-import RequireAuth from './components/Auth/RequireAuth';
-import AboutUsPage from './pages/AboutUsPage';
-import Contact from './pages/Contact';
-import CourseDescription from './pages/Course/CourseDescription';
-import CourseList from './pages/Course/CourseList';
-import CreateCourse from './pages/Course/CreateCourse';
-import Denied from './pages/Denied';
-import HomePage from './pages/HomePage';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
-import Signup from './pages/Signup';
-import Profile from './pages/User/Profile';
-
+import RequireAuth from "./Components/Auth/RequireAuth";
+import AboutUs from "./Pages/AboutUs"
+import Contact from "./Pages/Contact";
+import CourseDescription from "./Pages/Course/CourseDescription";
+import CourseList from "./Pages/Course/CourseList";
+import CreateCourse from "./Pages/Course/CreateCourse";
+import Denied from "./Pages/Denied";
+import HomePage from "./Pages/HomePage";
+import Login from "./Pages/Login";
+import NotFound from "./Pages/NotFound";
+import Signup from "./Pages/Signup";
+import EditProfile from "./Pages/User/EditProfile";
+import Profile from "./Pages/User/Profile";
 function App() {
+
   return (
     <>
-        <Routes>
-          <Route path="/" element={<HomePage />} ></Route>
-          <Route path="/about" element={<AboutUsPage />} ></Route>
-          <Route path='/courses' element={<CourseList/>}></Route>
-          
-          <Route path='/signup' element={<Signup/>}></Route>
-          <Route path='/login' element={<Login/>}></Route>
-          <Route path='/contact' element={<Contact/>} ></Route>
-          <Route path='/denied' element={<Denied/>}></Route>
-          <Route path='/course/description' element={<CourseDescription/>}></Route>
+      <Routes>
+        <Route path="/" element={<HomePage />} ></Route>
+        <Route path="/about" element={<AboutUs />} ></Route>
+        <Route path="/courses" element={< CourseList />} ></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/denied" element={<Denied />}></Route>
+        <Route path="/course/description" element={<CourseDescription />}></Route>
 
 
-          <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
-            <Route path='/course/create' element={<CreateCourse/>}></Route>
-          </Route>
-
-          <Route element={<RequireAuth allowedRoles={["ADMIN", "USER"]} />}>
-          <Route path="/user/profile" element={<Profile />}/>
-          <Route path="/user/editprofile" element={<EditProfile />}/>
-          </Route>
-
-          <Route path="*" element={<NotFound />} ></Route>
+        <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
+          <Route path="/course/create" element={<CreateCourse />}></Route>
         </Route>
 
+        <Route element={<RequireAuth allowedRoles={["ADMIN", "USER"]} />}>
+          <Route path="/user/profile" element={<Profile />}>
+          </Route>
+          <Route path="/user/profile" element={<Profile />}/>
+          <Route path="/user/editprofile" element={<EditProfile />}/>
+        </Route>
 
-        </Routes>
+        <Route path="*" element={<NotFound />} ></Route>
+      </Routes>
     </>
   )
 }
-export default App;
+
+export default App
